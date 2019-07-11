9db51a3361dc51099a4bb2982e0346da30973779
Cédric Belin <cedric@belin.io>
2019-07-11 08:53:01 +0200

    Moved the `Assertion->__get()` method to the `ChainableTrait` trait

--- lib/ChainableTrait.php
+++ lib/ChainableTrait.php
@@ -1,8 +1,44 @@
 <?php declare(strict_types=1);
 namespace PHPUnit\Expect;
 
-/** Provides chainable methods to improve the readability of assertions. */
-trait ChainTrait {
+/**
+ * Provides chainable methods to improve the readability of assertions.
+ * @property $this $and Chainable property to improve the readability of an assertion.
+ * @property $this $at Chainable property to improve the readability of an assertion.
+ * @property $this $be Chainable property to improve the readability of an assertion.
+ * @property $this $been Chainable property to improve the readability of an assertion.
+ * @property $this $but Chainable property to improve the readability of an assertion.
+ * @property $this $does Chainable property to improve the readability of an assertion.
+ * @property $this $has Chainable property to improve the readability of an assertion.
+ * @property $this $have Chainable property to improve the readability of an assertion.
+ * @property $this $is Chainable property to improve the readability of an assertion.
+ * @property $this $of Chainable property to improve the readability of an assertion.
+ * @property $this $same Chainable property to improve the readability of an assertion.
+ * @property $this $still Chainable property to improve the readability of an assertion.
+ * @property $this $that Chainable property to improve the readability of an assertion.
+ * @property $this $to Chainable property to improve the readability of an assertion.
+ * @property $this $which Chainable property to improve the readability of an assertion.
+ * @property $this $with Chainable property to improve the readability of an assertion.
+ */
+trait ChainableTrait {
+
+  /**
+   * Invokes the instance method with the specified name.
+   * @param string $name The name of the method to invoke.
+   * @return mixed The return value of the invoked method.
+   * @throws \InvalidArgumentException The specified method is not found.
+   */
+  function __get(string $name) {
+    static $reflection;
+    if (!$reflection) $reflection = new \ReflectionClass(static::class);
+
+    if ($reflection->hasMethod($name)) {
+      $method = $reflection->getMethod($name);
+      if ($method->isPublic() && !$method->getNumberOfRequiredParameters()) return $this->$name();
+    }
+
+    throw new \InvalidArgumentException("The specified method is not found: $name()");
+  }
 
   /**
    * Chainable method to improve the readability of an assertion.
